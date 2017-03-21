3dda2cb14f2e91ee235236cdea264763988b099d
gpdno <gdeninno@suncoastde.com>
2017-03-20 21:20:59 -0400

    finished article upload

--- app/models/artifact.rb
+++ app/models/artifact.rb
@@ -1,4 +1,5 @@
 class Artifact < ApplicationRecord
+  before_save :upload_to_s3
   belongs_to :project
 
   validates_presence_of :name, :upload
@@ -12,14 +13,16 @@ class Artifact < ApplicationRecord
 
   def uploaded_file_size
     if upload
-      errors.add(:upload, "File size must be less than #{self.class::MAX_FILESIZE}") 
-      unless upload.size <= self.class::MAX_FILESIZE
+      errors.add(:upload, "File size must be less than #{self.class::MAX_FILESIZE}") unless upload.size <= self.class::MAX_FILESIZE
     end
   end
 
   def upload_to_s3
-    s3 = Aws::S3::Resource.new(region:'us-west-2')
-    obj = s3.bucket('bucket-name').object('key')
-    obj.upload_file('/path/to/source/file')
-  
+    s3 = Aws::S3::Resource.new(region: ENV['AWS_REGION'])
+    tenant_name = Tenant.find(Thread.current[:tenant_id]).name
+    obj = s3.bucket(ENV['S3_BUCKET_NAME']).object("#{tenant_name}/#{upload.original_filename}")
+    obj.upload_file(upload.path, acl:'public-read')
+    self.key = obj.public_url
+  end
+
 end
